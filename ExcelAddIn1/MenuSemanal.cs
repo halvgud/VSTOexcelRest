229c9bfe991d6eeced74617e25b39947f60fe9f5
Emanuel Vega <evo18251993@gmail.com>
2017-04-04 11:33:48 -0600

    nuevo menu

--- ExcelAddIn1/MenuSemanal.cs
+++ ExcelAddIn1/MenuSemanal.cs
@@ -19,54 +19,13 @@ namespace ExcelAddIn1
             
         }
 
-        private void Btrecetasemanal_Click(object sender, EventArgs e)
-        {
-
-        }
-
+       
         private void Dtpikerinicio_ValueChanged(object sender, EventArgs e)
         {
            
         }
 
-        private void Dtpikerfinal_ValueChanged(object sender, EventArgs e)
-        {
-           
-            DateTime inicio = Dtpikerinicio.Value;
-            DateTime final = Dtpikerfinal.Value;
-            
-            int numerodias = ((final - inicio).Days);
-            DateTime dd = DateTime.Now;
-            string fecha = dd.ToString("yyyy-M-d");
-            lbDiaSemana.Items.Add(fecha);
-            lbDiaSemana.Items.Add(Dtpikerfinal.Value.Day).ToString("dddd");
-
-
-
-      
-            //for (int i = 0; i <= numerodias; i++)
-            //{
-
-            //}
-
-            //int prueba = (final - inicio);
-            ////Convert.ToString() ;
-            ////lbDiaSemana.Items.Add(numerodias);
-            //Convert.ToString(numerodias);
-            //lbDiaSemana.Items.Add(Dtpikerinicio.Value).ToString("dddd");
-
-
-
-            //DateTime dateValue = new DateTime(2017,4 , 3);
-            //lbDiaSemana.Items.Add(dateValue.ToString("dddd"));
-            //lbDiaSemana.Items.Add(numerodias);
-
-            //string[] dias = fecha.Split('-');
-            //int calis = fecha[1];
-            //lbDiaSemana.Items.Add(calis).ToString();
-        }
-
-        private void lbDiaSemana_SelectedIndexChanged(object sender, EventArgs e)
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
         }

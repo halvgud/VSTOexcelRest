bdd6dee63827c243a235e463984bffd2487fa622
manolete9324 <emanuel.vega.ornelas.93@gmail.com>
2017-05-08 11:03:17 -0600

    prueba nueva

--- ExcelAddIn1/Congelados.cs
+++ ExcelAddIn1/Congelados.cs
@@ -34,7 +34,7 @@ namespace ExcelAddIn1
         private List<Receta.Congelados> _listaplatillo;
 
         private List<Receta.Congelados> _listaagregarcongelado2;
-  
+  //modificacion
 
         //private List<Respuesta.CbGenerico> _listaplatillo; 
 
